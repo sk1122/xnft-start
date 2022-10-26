3bb90a34aa5fface9689cb3a317f2bbcef07cd93
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-10-26 05:58:41 +0530

    .

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactXnft, { Tab, View } from "react-xnft";
+
+const App = () => {
+    return (
+        <View style={{
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            Hello World
+        </View>
+    )
+}
+
+export default App
---
+++ src/screens/Main.tsx
@@ -0,0 +1,26 @@
+// import { useState } from "react";
+import { View, Text } from "react-xnft";
+
+export const Main = () => {
+    // const [text, setText] = useState('1')
+    
+    return (
+      <View
+        style={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: "#000",
+          }}
+        >
+          Satyam
+        </Text>
+      </View>
+    );
+}
